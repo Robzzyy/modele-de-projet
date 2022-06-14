25ce47d7117bc409533701540e03b0ada0d7175b
Lucas <lucas.robert@lepoles.org>
2022-06-14 16:49:50 +0200

    hihhu

---
+++ assets/js/apps.js
@@ -0,0 +1,9 @@
+
+
+var slideImg = [
+    "https://th.bing.com/th/id/OIP.YJHbqAIcAQ34U4PBcxRUigHaE7?pid=ImgDet&rs=1",
+    "https://www.modern-notoriety.com/wp-content/uploads/2019/03/coa.jpg",
+    "https://th.bing.com/th/id/OIP.oTnfOXbB-Kt9Sa2TO6Y0eQHaFS?pid=ImgDet&rs=1",
+
+]
+ slideImg = new MySlider($('#slider'),slideImg)
